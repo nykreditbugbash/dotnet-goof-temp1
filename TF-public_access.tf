ba69622dffaceab3c7a860854886672800fc4e36
Arne Kratz <ARKR@nykredit.dk>
2025-08-20 11:46:54 +0200

    Revert "enabled logging"
    This reverts commit b8bff93c0d231032261793a4c31c26b6520524c5.

--- TF-public_access.tf
+++ TF-public_access.tf
@@ -9,10 +9,6 @@ resource "aws_db_instance" "db" {
   username             = "foo"
   password             = "foobarbaz"
   parameter_group_name = "default.mysql5.7"
-  storage_encrypted = true
-  iam_database_authentication_enabled = true
-  backup_retention_period = 1
-
 }
 
 resource "aws_dms_replication_instance" "db" {
@@ -65,9 +61,6 @@ resource "aws_mq_broker" "mq" {
     username = "ExampleUser"
     password = "MindTheGap"
   }
-  logs {
-    general = true
-  }
 }
 
 resource "aws_neptune_cluster_instance" "neptune" {
@@ -97,9 +90,4 @@ resource "aws_redshift_cluster" "redshift" {
   node_type          = "dc1.large"
   cluster_type       = "single-node"
   publicly_accessible = false
-  encrypted = true
-  enable = true
-  logging {
-  enable = true
-  }
 }
